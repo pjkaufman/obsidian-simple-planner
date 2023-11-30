f955afcfe1e554b2b67db2f724e508dbdc102aaf
Peter Kaufman <PeterJamesKaufman@gmail.com>
2023-11-30 15:59:02 -0500

    added a couple more types and settings setup

--- src/types.ts
+++ src/types.ts
@@ -1,26 +1,35 @@
 export type CalendarEvent = {
-    name: string,
-    description: string | undefined,
-    calendar: string,
-    occurrenceInfo: OccurrenceInfo,
+  name: string,
+  description: string | undefined,
+  calendar: string,
+  occurrenceInfo: OccurrenceInfo,
 };
 
 export type OccurrenceInfo = {
-    time: string | undefined,
-    months: number[],
-    day: number,
-    daysOfWeek: number[],
-    weeks: number[],
-    isEaster: boolean,
-    isElectionDay: boolean,
-    isGoodFriday: boolean,
-    isPalmSunday: boolean,
+  time: string | undefined,
+  months: number[],
+  day: number,
+  daysOfWeek: number[],
+  weeks: number[],
+  isEaster: boolean,
+  isElectionDay: boolean,
+  isGoodFriday: boolean,
+  isPalmSunday: boolean,
+};
+
+export type Calendar = {
+  name: string,
+  color: string,
 };
 
 export interface SimplePlannerSettings {
-	mySetting: string;
+  events: CalendarEvent[];
+	calendars: Calendar[];
+  plannerBaseFolderPath: string;
 }
 
 export const DEFAULT_SETTINGS: SimplePlannerSettings = {
-	mySetting: 'default'
-}
+	events: [],
+  calendars: [],
+  plannerBaseFolderPath: '',
+};
